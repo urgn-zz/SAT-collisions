ff9a22566c561f0d22dcd1d5179641735d0e3a50
Yuriy Raichynets <Yuriy.Raichynets@netent.com>
2016-05-04 11:28:12 +0200

    small refactor

--- Shape.js
+++ Shape.js
@@ -1,8 +1,8 @@
-function Shape (points, x, y, color, stroke) {
+function Shape(points, x, y, color, stroke) {
     this.x = x || 0;
     this.y = y || 0;
-    this.points = points.map(function(el){
-        return {x: el[0], y: el[1]}
+    this.points = points.map(function (el) {
+        return {x: el[0], y: el[1]};
     });
     this.color = color || "rgba(0,0,0,0)";
     this.stroke = stroke || "black";
@@ -10,18 +10,18 @@ function Shape (points, x, y, color, stroke) {
     this.getMedians();
 }
 
-Shape.prototype.draw = function(ctx) {
+Shape.prototype.draw = function (ctx) {
     var p = this.points;
     ctx.save();
     ctx.fillStyle = this.color;
     ctx.strokeStyle = this.stroke;
     ctx.lineWidth = 3;
     ctx.translate(this.x, this.y);
-    p.forEach(function(point, i) {
+    p.forEach(function (point, i) {
         if (i === 0) {
             ctx.beginPath();
             ctx.moveTo(point.x, point.y);
-        } else if ( i === (p.length - 1)) {
+        } else if (i === (p.length - 1)) {
             ctx.lineTo(point.x, point.y);
             ctx.lineTo(p[0].x, p[0].y);
             ctx.stroke();
@@ -34,13 +34,17 @@ Shape.prototype.draw = function(ctx) {
     ctx.restore();
 };
 
-Shape.prototype.drawNormals = function(ctx) {
+Shape.prototype.drawNormals = function (ctx) {
     var m = this.medians,
         n = this.normals;
 
+    ctx.save();
+
     ctx.lineWidth = 5;
-    ctx.strokeStyle = '#003300';
-    ctx.fillStyle = 'green';
+    ctx.strokeStyle = "#003300";
+    ctx.fillStyle = "green";
+
+    ctx.translate(this.x, this.y);
 
     m.forEach(function (point) {
         ctx.beginPath();
@@ -50,18 +54,20 @@ Shape.prototype.drawNormals = function(ctx) {
         ctx.closePath();
     });
 
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = "red";
     ctx.lineWidth = 5;
-    ctx.strokeStyle = '#003300';
+    ctx.strokeStyle = "#003300";
 
-    n.forEach(function (point,i) {
+    n.forEach(function (point, i) {
         ctx.beginPath();
         ctx.moveTo(m[i].x, m[i].y);
-        ctx.lineTo(m[i].x + point.x*25, m[i].y + point.y*25);
+        ctx.lineTo(m[i].x + point.x * 25, m[i].y + point.y * 25);
         ctx.fill();
         ctx.stroke();
         ctx.closePath();
     });
+
+    ctx.restore();
 };
 
 Shape.prototype.getNormals = function () {
@@ -69,13 +75,13 @@ Shape.prototype.getNormals = function () {
         crt, nxt, l, x1, y1;
 
     this.normals = [];
-    for(var i = 0; i < p.length; i++) {
+    for (var i = 0; i < p.length; i++) {
         crt = p[i];
         nxt = p[i + 1] || p[0];
         x1 = (nxt.y - crt.y);
         y1 = -(nxt.x - crt.x);
-        l = Math.sqrt(x1*x1 + y1*y1);
-        this.normals.push({x: x1/l, y: y1/l});
+        l = Math.sqrt(x1 * x1 + y1 * y1);
+        this.normals.push({x: x1 / l, y: y1 / l});
     }
 };
 
@@ -85,40 +91,40 @@ Shape.prototype.getMedians = function () {
 
     this.medians = [];
 
-    for(var i = 0; i < p.length; i++) {
+    for (var i = 0; i < p.length; i++) {
         crt = p[i];
         nxt = p[i + 1] || p[0];
-        this.medians.push({x: (crt.x + nxt.x)/2, y: (crt.y + nxt.y)/2});
+        this.medians.push({x: (crt.x + nxt.x) / 2, y: (crt.y + nxt.y) / 2});
     }
 };
 
-Shape.prototype.move = function(x, y) {
+Shape.prototype.move = function (x, y) {
     this.x = x;
     this.y = y;
 };
 
-Shape.prototype.checkCollision = function(shape){
+Shape.prototype.checkCollision = function (shape) {
     var me = this,
         p1, p2;
 
-    return me.normals.concat(shape.normals).every(function(v){
+    return me.normals.concat(shape.normals).every(function (v) {
         p1 = me.project(v);
         p2 = shape.project(v);
         return (((p1.min <= p2.max) && (p1.max >= p2.min)) ||
-                 (p2.min >= p1.max) && (p2.max >= p1.min));
+        (p2.min >= p1.max) && (p2.max >= p1.min));
     });
 };
 
-Shape.prototype.project = function(vector) {
+Shape.prototype.project = function (vector) {
     var me = this,
         p = this.points,
         min = Infinity, max = -Infinity,
         x, y, proj;
 
-    p.forEach(function(p){
+    p.forEach(function (p) {
         x = me.x + p.x;
         y = me.y + p.y;
-        proj = (x*vector.x + y*vector.y);
+        proj = (x * vector.x + y * vector.y);
         min = proj < min ? proj : min;
         max = proj > max ? proj : max;
     });
--- main.js
+++ main.js
@@ -4,33 +4,34 @@ var canvas = document.getElementById("scene"),
     h = window.innerHeight,
     isDown = false,
     list = {
-        triangle: new Shape([[30,0],[60,60],[0,60]], 50, 50),
-        rhombus: new Shape([[50, 0], [100,50], [50, 100], [0, 50]], 200, 50)
+        triangle: new Shape([[30, 0], [60, 60], [0, 60]], 50, 50),
+        rhombus: new Shape([[50, 0], [100, 50], [50, 100], [0, 50]], 200, 50)
     },
-    render = function() {
+    render = function () {
         ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(0,0,w,h);
+        ctx.fillRect(0, 0, w, h);
         for (var shape in list) {
             if (list.hasOwnProperty(shape)) {
                 list[shape].draw(ctx);
+                list[shape].drawNormals(ctx);
             }
         }
         requestAnimationFrame(render);
     };
 
-canvas.addEventListener("mousedown", function(e) {
+canvas.addEventListener("mousedown", function (e) {
     isDown = true;
     checkFigures({x: e.clientX, y: e.clientY});
 });
 
-canvas.addEventListener("mouseup", function() {
+canvas.addEventListener("mouseup", function () {
     isDown = false;
     for (var figure in list) {
         list[figure].moving = false;
     }
 });
 
-canvas.addEventListener("mousemove", function(e) {
+canvas.addEventListener("mousemove", function (e) {
     var fig;
     if (isDown) {
         for (var figure in list) {
@@ -44,13 +45,12 @@ canvas.addEventListener("mousemove", function(e) {
                     list.triangle.stroke = 'black';
                     list.rhombus.stroke = 'black';
                 }
-
             }
         }
     }
 });
 
-function checkFigures (mp) {
+function checkFigures(mp) {
     for (var fig in list) {
         if (isPointInPoly(list[fig].points, {x: mp.x - list[fig].x, y: mp.y - list[fig].y})) {
             list[fig].moving = true;
@@ -62,8 +62,8 @@ function checkFigures (mp) {
     }
 }
 
-function isPointInPoly(poly, pt){
-    for(var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+function isPointInPoly(poly, pt) {
+    for (var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
         ((poly[i].y <= pt.y && pt.y < poly[j].y) || (poly[j].y <= pt.y && pt.y < poly[i].y))
         && (pt.x < (poly[j].x - poly[i].x) * (pt.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x)
         && (c = !c);
